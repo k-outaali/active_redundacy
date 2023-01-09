a213096a8c2a9b975fb5b9b102e6410bd3f88461
khalid outaali <khalid.outaali@etudiant.univ-brest.fr>
2023-01-09 20:54:28 +0100

    implemented what was missing

--- redundancy.c
+++ redundancy.c
@@ -3,75 +3,127 @@
 
 #include  <stdio.h>			
 #include  <stdlib.h>			
-#include  <string.h>			
+#include  <string.h>		
+#include  <semaphore.h>	
 
 #include  "redundancy.h"			
 #include  "returncode.h"			
 
 
+int id = -1;
 
 
-
-int pthread_redundancy_attr_destroy(struct pthread_redundancy_attr_t *a) {
+int pthread_redundancy_attr_destroy(struct pthread_redundancy_attr *a){
    return 0;
 }
 
 
-int pthread_redundancy_attr_init(struct pthread_redundancy_attr_t *a) {
+int pthread_redundancy_attr_init(struct pthread_redundancy_attr *a){
+
    a->size = 1;
    a->priority = 0;
    return 0;
 }
 
+int pthread_redundancy_attr_setpriority(struct pthread_redundancy_attr *a, int prio){
 
-int pthread_redundancy_destroy(struct pthread_redundancy_t* r) {
+  a->priority = prio;
+  return 0;
+}
+
+int pthread_redundancy_attr_getpriority(struct pthread_redundancy_attr a){
+
+  return a.priority;
+}
+
+int pthread_redundancy_attr_setsize(struct pthread_redundancy_attr *a, int s){
+
+  a->size = s;
+  return 0;
+}
+
+int pthread_redundancy_attr_getsize(struct pthread_redundancy_attr a){
+
+  return a.size;
+}
+
+int pthread_redundancy_destroy(struct pthread_redundancy *r) {
 
   int i;
-  int status;
+  int ret;
 
 
   // Initially, cont=1 and threads are in a while loop until 
   // become cont=1
   //
-  r->cont=0;
+  //r->cont=0;
 
   // Wakeup threads from their private semaphore to allow them
   // to finish
   //
-  for(i=0; i<r->attr.size; i++)
-    {
-    status=sem_post(&r->waitfor_barrier[i]);
-    if(status!=0)
-        {
-           returncode("pthread_redundancy_destroy sem_post",status);
-           return status;
-        }
+  for(i=0; i<r->attr.size; i++){
+    ret = sem_post(&r->wait_for_barrier[i]);
+    if(ret!=0){
+      returncode("pthread_redundancy_destroy sem_post",ret);
+      return -1;
     }
+  }
 
   return 0;
 }
 
 
+void *redudancy_thread(void *arg){
+
+  id++;
+  int ret;
+  pthread_redundancy_t *r = arg;
+
+  while(1) {
+    ret = sem_wait(&(r->wait_for_barrier[id]));
+    if(ret != 0){
+      returncode("sem_wait failed", ret);
+    }
+    /*
+    if (cont==0)
+      pthread_exit(NULL);
+    */
+    r->results[id] = r->voters[id](arg);
+    ret = sem_post(&(r->completion_barrier[id]));
+    if(ret != 0){
+      returncode("sem_post failed", ret);
+    }
+  }
+}
 
 
-
-int pthread_redundancy_init(struct pthread_redundancy_t* r, struct pthread_redundancy_attr_t a, voter_type v [], void* arg) {
+int pthread_redundancy_init(struct pthread_redundancy* r, struct pthread_redundancy_attr attr, voter_t v[], void* arg) {
   
-  int i;
-
-  r->attr = a;
+  int i, ret;
+  id = -1;
+  r->attr = attr;
   r->arg = arg;
-  r->cont = 1;
-  for(i = 0; i < r->attr.size; i++) 
-    {
+  //r->cont = 1;
+  for(i = 0; i < r->attr.size; i++) {
     r->voters[i] = v[i];
 
-    ***
+    ret = sem_init(&(r->wait_for_barrier[i]), 0, 0);
+    if(ret != 0){
+      returncode("sem_init failed", ret);
     }
 
-    ***
+    ret = sem_init(&(r->completion_barrier[i]), 0, 0);
+    if(ret != 0){
+      returncode("sem_init failed", ret);
+    }
 
-    return 0;
+    ret = pthread_create(&(r->tid[i]), NULL, redudancy_thread, (void *) r);
+    if(ret != 0){
+      returncode("pthread_create failed", ret);
+    }
+
+  }
+  return 0;
 }
 
 
--- redundancy.h
+++ redundancy.h
@@ -11,36 +11,37 @@
 typedef int (*voter_type) (void*);
 
 
-typedef struct pthread_redundancy_attr_t {
+typedef struct pthread_redundancy_attr {
   int priority;
   int size;
 } pthread_redundancy_attr_t;
 
 
-typedef struct pthread_redundancy_t{
-  sem_t      waitfor_barrier[CONFIGURE_REDUNDANCY_MAXIMUM_VOTING];
+typedef struct pthread_redundancy{
+  sem_t      wait_for_barrier[CONFIGURE_REDUNDANCY_MAXIMUM_VOTING];
   sem_t      completion_barrier[CONFIGURE_REDUNDANCY_MAXIMUM_VOTING];
   int        results[CONFIGURE_REDUNDANCY_MAXIMUM_VOTING];
   voter_type voters [CONFIGURE_REDUNDANCY_MAXIMUM_VOTING];
   void*      arg;
-  struct     pthread_redundancy_attr_t attr;
+  struct     pthread_redundancy_attr attr;
+  pthread_t tid[CONFIGURE_REDUNDANCY_MAXIMUM_VOTING];
   // Extend this struct if needed! 
 } pthread_redundancy_t;
 
 
 
 
-extern int pthread_redundancy_attr_init(struct pthread_redundancy_attr_t *a);
-extern int pthread_redundancy_attr_setpriority(struct pthread_redundancy_attr_t *a, int prio);
-extern int pthread_redundancy_attr_getpriority(struct pthread_redundancy_attr_t a);
-extern int pthread_redundancy_attr_setsize(struct pthread_redundancy_attr_t *a, int s);
-extern int pthread_redundancy_attr_getsize(struct pthread_redundancy_attr_t a);
-extern int pthread_redundancy_attr_destroy(struct pthread_redundancy_attr_t *a);
+extern int pthread_redundancy_attr_init(struct pthread_redundancy_attr *a);
+extern int pthread_redundancy_attr_setpriority(struct pthread_redundancy_attr *a, int prio);
+extern int pthread_redundancy_attr_getpriority(struct pthread_redundancy_attr a);
+extern int pthread_redundancy_attr_setsize(struct pthread_redundancy_attr *a, int s);
+extern int pthread_redundancy_attr_getsize(struct pthread_redundancy_attr a);
+extern int pthread_redundancy_attr_destroy(struct pthread_redundancy_attr *a);
 
 
-extern int pthread_redundancy_init(struct pthread_redundancy_t* r, struct pthread_redundancy_attr_t a, voter_type v [], void* arg);
-extern int pthread_redundancy_vote(struct pthread_redundancy_t* r, int* result);
-extern int pthread_redundancy_destroy(struct pthread_redundancy_t* r);
+extern int pthread_redundancy_init(struct pthread_redundancy* r, struct pthread_redundancy_attr a, voter_type v [], void* arg);
+extern int pthread_redundancy_vote(struct pthread_redundancy* r, int* result);
+extern int pthread_redundancy_destroy(struct pthread_redundancy* r);
 
 
 
